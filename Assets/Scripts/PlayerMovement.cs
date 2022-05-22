a6a2a47591b77a3ce7e0ff7fa2521f9a93d9c74f
Aaron Bell <abell730b@gmail.com>
2022-05-22 12:12:42 +1000

    Deleted prototype wall sprites from scene. Added OnFire() event for player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,8 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     bool isAlive;
 
+    // events
+
     void Awake()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
@@ -31,18 +33,25 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    void MovePlayer()
-    {
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, moveInput.y * runSpeed);
-        myRigidBody.velocity = playerVelocity;
-    }
-
     void OnMove(InputValue value)
     {
         if (!isAlive)
             return;
 
         moveInput = isAlive ? value.Get<Vector2>() : new Vector2(0, 0);
+    }
+
+    void OnFire(InputValue value)
+    {
+        Debug.Log("BOO!");
+    }
+
+    // private methods
+
+    void MovePlayer()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, moveInput.y * runSpeed);
+        myRigidBody.velocity = playerVelocity;
     }
 
     
