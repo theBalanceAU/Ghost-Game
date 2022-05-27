4c546f99afa9fc0755864c45e240c187ec9588aa
Aaron Bell <abell730b@gmail.com>
2022-05-27 13:07:34 +1000

    Added new animated enemies (Gal, Bat and Skelly). Added waypoint following to EnemyBehavior with Gizmos in editor to show patrol path.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,29 +4,108 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    [Header("Scare Behavior")]
     [SerializeField] bool canScare;
     [SerializeField] float disappearAfterSeconds = 2f;
-    
-    float runSpeed = 5f;
 
+    [Header("Enemy Patrol Path")]
+    [SerializeField] bool hideInEditor;
+    [SerializeField] List<Vector2> waypoints;
+
+    float runSpeed = 2f;
     bool isScared;
+    int nextWaypointIndex = 0;
+    Vector2 initialRuntimePosition;
 
     Rigidbody2D myRigidBody;
     Animator myAnimator;
     Collider2D myCollider;
+
+    //List<Vector2> worldWaypoints;
 
     private void Awake()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCollider = GetComponent<Collider2D>();
+
+        initialRuntimePosition = transform.position;
+
+        // if (waypoints != null && waypoints.Count > 0)
+        // {
+        //     worldWaypoints = new List<Vector2>();
+        //     for (int i = 0; i < waypoints.Count; i++)
+        //     {
+        //         worldWaypoints.Add(transform.position + (Vector3)waypoints[i]);
+        //     }
+        // }
     }
 
     void Update()
     {
-        if (isScared)
+        FollowWaypoints();
+    }
+
+    void FollowWaypoints()
+    {
+        if (isScared || waypoints == null || waypoints.Count == 0)
+            return;
+
+        const float tolerance = 0.01f;
+
+        Vector3 nextWaypoint = initialRuntimePosition;
+        if (nextWaypointIndex >= 0)
+            nextWaypoint = (Vector3)waypoints[nextWaypointIndex];
+
+        
+        Vector3 direction = -(transform.position - nextWaypoint);
+        direction.Normalize();
+
+        Debug.Log($"index={nextWaypointIndex}");
+        Debug.Log($"position={(Vector2)transform.position}");
+        Debug.Log($"nextWaypoint={(Vector2)nextWaypoint}");
+        Debug.Log($"direction={(Vector2)direction}");
+
+        myRigidBody.velocity = direction * runSpeed;
+
+        // if we reached destination, point to next waypoint
+        Collider2D[] objectsAtWaypoint = Physics2D.OverlapCircleAll(nextWaypoint, tolerance);
+        if (objectsAtWaypoint.Length > 0)
         {
-            // TODO
+            for (int i = 0; i < objectsAtWaypoint.Length; i++)
+            {
+                if (objectsAtWaypoint[i] == myCollider)
+                {
+                    nextWaypointIndex++;
+                    if (nextWaypointIndex >= waypoints.Count)
+                        nextWaypointIndex = -1;
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (hideInEditor)
+            return;
+
+        const float waypointRadius = 0.2f;
+        Vector3 startPosition = transform.position;
+        if (initialRuntimePosition != Vector2.zero)
+            startPosition = initialRuntimePosition;
+
+        // show waypoints in Unity editor
+        if (waypoints != null && waypoints.Count > 0)
+        {
+            Vector3 previousWaypoint = startPosition + Vector3.zero;
+            Gizmos.DrawWireSphere(startPosition, waypointRadius);
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                Vector3 nextWaypoint = (Vector3)waypoints[i];
+                Gizmos.DrawWireSphere(nextWaypoint, waypointRadius);
+                Gizmos.DrawLine(previousWaypoint, nextWaypoint);
+                previousWaypoint = nextWaypoint;
+            }
         }
     }
 
